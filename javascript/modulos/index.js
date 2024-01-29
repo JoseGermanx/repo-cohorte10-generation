57229e1c883fa84fb672ccb80acb7052a98956ab
Jose Germán Martínez <jgermanmm@gmail.com>
2024-01-29 14:06:12 -0300

    modulos es6

---
+++ javascript/modulos/index.js
@@ -0,0 +1,16 @@
+
+
+// ES6 javascrip 2015 - moderno
+// export (crear el módulo)
+// import (consumir el módulo)
+
+// commonJs antes del 2015
+// module.exports (creacion del modulo)
+// require("./variables.js")
+
+
+import * as m from "./variables.js"
+
+console.log(m.day)
+
+console.log(m.month)
