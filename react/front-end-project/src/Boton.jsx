34f20b377b82a9aa3f509d8c8fed713cb53e0aa4
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-01 08:41:08 -0300

    instalacion de react - vite

---
+++ react/front-end-project/src/Boton.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Boton = () => {
+  return (
+    <div>
+    <button>Esto es un boton</button>    
+    </div>
+  )
+}
+
+export default Boton
